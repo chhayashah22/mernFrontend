0e073bdae1c399a12c558ed72778ccde7d39e349
chhayashah22 <shahchhaya607@gmail.com>
2025-02-23 15:23:06 +0530

    value

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,8 @@ import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import api from "../api";
+
+import axios from "axios";
 export default function Login() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -20,7 +21,7 @@ export default function Login() {
     console.log("Form Data:", formData);
 
     try {
-      const response = await api.post("/api/login/user-login", formData);
+      const response = await axios.post("/api/login/user-login", formData);
       console.log(response);
       const { token, name: Username } = response.data;
 
