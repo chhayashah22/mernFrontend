d32adb8111f49e63517bff7b1afb21bc71b38435
chhayashah22 <shahchhaya607@gmail.com>
2025-02-23 13:44:29 +0530

    last changed remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./components/Landing";
-import Login from "./components/Login";
-import DashboardLayout from "./components/DashboardLayout";
-import Transaction from "./components/Transaction";
+import Login from './components/Login';
+import DashboardLayout from './components/DashboardLayout';
+import Transaction from './components/Transaction';
 import Dashboard from "./components/Dashboard";
-import Donation from "./components/Donation";
-import Register from "./components/Register";
-import { Outlet } from "react-router-dom";
+import Donation from './components/Donation'
+import Register from './components/Register'
 
 // Mock Authentication Check
 const isAuthenticated = () => !!localStorage.getItem("token");
@@ -19,26 +18,25 @@ const ProtectedRoute = ({ children }) => {
 
 export default function AppRoutes() {
   return (
-    <Router> {/* ✅ Change HashRouter to BrowserRouter */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register/>}/>
 
-        {/* ✅ Protected Dashboard Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Dashboard />} /> {/* ✅ Default route */}
-          <Route path="transactions" element={<Transaction />} />
-          <Route path="donation" element={<Donation />} />
-        </Route>
-      </Routes>
-    </Router>
+      {/* Protected Dashboard Routes */}
+      <Route
+        path="/dashboard/*"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        {/* Default Dashboard Route (Fixes the issue) */}
+        <Route index element={<Dashboard />} />
+        <Route path="transactions" element={<Transaction />} />
+        <Route path="donation" element={<Donation/>}/>
+      </Route>
+    </Routes>
   );
 }
